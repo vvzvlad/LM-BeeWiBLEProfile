752b5b153341501e6e6e63f41a501f5612ddb31d
vvzvlad <vvzvlad@gmail.com>
2015-12-13 05:39:55 +0300

    Что-то добавил.
    Что добавил - хз. Спать хочу.

--- beewi-smartledcolorbulb.lua
+++ beewi-smartledcolorbulb.lua
@@ -4,57 +4,31 @@ return {
   default_name = 'BeeWi Bulb',
   objects = {
     {
-      id = 'raw',
-      name = 'RAW data',
-      datatype = dt.string
-    },
-    {
       id = 'power',
       name = 'Power',
       datatype = dt.bool
+    },
+    {
+      id = 'white',
+      name = 'White',
+      datatype = dt.scale
     },
     {
       id = 'color',
       name = 'Color',
       datatype = dt.rgb
     },
+    {
+      id = 'brightness',
+      name = 'Brightness',
+      datatype = dt.scale
+    },
   },
-  init = function(device)
-    local objects = device.objects
-  end,
-  read = function(device)
-    local sock
-    values = {}
-
-    sock = ble.sock()
-
-    ble.settimeout(sock, 30)
-    res = ble.connect(sock, device.mac)
-    if res then
-      value_0x24 = ble.sockreadhnd(sock, 0x24) or ''
-      if (#value_0x24 == 5) then   
-        status = true
-        if (value_0x24:byte(1) == 0) then
-          values.power = false
-        elseif (value_0x24:byte(1) == 1) then
-          values.power = true
-        end
-
-        values.raw = value_0x24
-        
-      end
-    end
-
-    ble.close(sock)
-   
-    return status, values
-  end,
   write = function(device, object, value)
     local sock
     sock = ble.sock()
     ble.settimeout(sock, 30)
     res = ble.connect(sock, device.mac)
-
 
     if (object.id == 'power') then
       if res then
@@ -72,11 +46,44 @@ return {
         red = bit.band(bit.rshift(value, 16), 0xFF)
         green = bit.band(bit.rshift(value, 8), 0xFF)
         blue = bit.band(value, 0xFF)
-        if (red == 255 and green == 255 and blue == 255) then 
-          ble.sockwritecmd(sock, 0x21, 0x55, 0x14, 0xFF, 0xFF, 0xFF, 0x0D, 0x0A)
-        else
-          ble.sockwritecmd(sock, 0x21, 0x55, 0x13, red, green, blue, 0x0D, 0x0A)
+        ble.sockwritecmd(sock, 0x21, 0x55, 0x13, red, green, blue, 0x0D, 0x0A)
+      end
+    end
+
+    if (object.id == 'white') then
+      if res then
+        ble.sockwritecmd(sock, 0x21, 0x55, 0x14, 0xFF, 0xFF, 0xFF, 0x0D, 0x0A)
+        if (value >= 0 and value < 10) then bite = 0x02
+        elseif (value >= 10 and value < 20) then bite = 0x03 
+        elseif (value >= 20 and value < 30) then bite = 0x04
+        elseif (value >= 30 and value < 40) then bite = 0x05
+        elseif (value >= 40 and value < 50) then bite = 0x06
+        elseif (value >= 50 and value < 60) then bite = 0x07
+        elseif (value >= 60 and value < 70) then bite = 0x08
+        elseif (value >= 70 and value < 80) then bite = 0x09
+        elseif (value >= 80 and value < 90) then bite = 0x0A 
+        elseif (value >= 90 and value <= 100) then bite = 0x0B
         end
+        log(bite)
+        ble.sockwritecmd(sock, 0x21, 0x55, 0x11, bite, 0x0D, 0x0A)
+      end
+    end
+
+    if (object.id == 'brightness') then
+      if res then
+        if (value >= 0 and value < 10) then bite = 0x02
+        elseif (value >= 10 and value < 20) then bite = 0x03 
+        elseif (value >= 20 and value < 30) then bite = 0x04
+        elseif (value >= 30 and value < 40) then bite = 0x05
+        elseif (value >= 40 and value < 50) then bite = 0x06
+        elseif (value >= 50 and value < 60) then bite = 0x07
+        elseif (value >= 60 and value < 70) then bite = 0x08
+        elseif (value >= 70 and value < 80) then bite = 0x09
+        elseif (value >= 80 and value < 90) then bite = 0x0A 
+        elseif (value >= 90 and value <= 100) then bite = 0x0B
+        end
+        log(bite)
+        ble.sockwritecmd(sock, 0x21, 0x55, 0x12, bite, 0x0D, 0x0A)
       end
     end
 
